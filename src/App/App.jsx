e8c8a5154a318654a19c7929914d3fd04eb62cd7
Georgiy Filippov <g.filippov777@gmail.com>
2023-05-29 23:45:37 +0500

    feat: add SearchPanel

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import AppInfo from '../components/AppInfo'
+import SearchPanel from '../components/SearchPanel'
 // import styles from './App.module.css'
 
 const App = () => {
   return (
-    <main className='container-lg p-4'>
+    <main className='container-lg d-flex flex-column p-4 gap-4'>
       <AppInfo />
+
+      <section>
+        <SearchPanel />
+      </section>
     </main>
   )
 }
