a1678d5215771d879dfeb0c83c1243b632ed9556
Georgiy Filippov <g.filippov777@gmail.com>
2023-05-29 14:02:07 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# template_CRA-React17-JS
+
+- minimal template – nothing serious
+- template based on CRA 4 / React 17 / NodeJS 16
+- codegen to create Components
---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './App.module.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className={styles.heading}>React app</h1>
+    </div>
+  )
+}
+
+export default App
