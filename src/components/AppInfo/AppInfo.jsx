8920d9381aeaf1140a1a06156cd1eaad54c230ed
Georgiy Filippov <g.filippov777@gmail.com>
2023-05-30 03:24:49 +0500

    feat: add EmployeesAddForm

--- src/components/AppInfo/AppInfo.jsx
+++ src/components/AppInfo/AppInfo.jsx
@@ -4,9 +4,9 @@ import styles from './AppInfo.module.css'
 const AppInfo = () => {
   return (
     <section className={styles.wrapper}>
-      <h1 className='mb-4'>Staff Accounting at company NextReact</h1>
-      <h3 className='fw-normal fst-italic'>Total Employees: 77</h3>
-      <h3 className='fw-normal fst-italic'>Employees on promotion: 55</h3>
+      <h1 className='text-center mb-4'>Staff Accounting at company NextReact</h1>
+      <h4 className='fw-normal fst-italic'>Total Employees: 77</h4>
+      <h4 className='fw-normal fst-italic'>Employees on promotion: 55</h4>
     </section>
   )
 }
