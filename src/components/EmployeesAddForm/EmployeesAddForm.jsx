4b8b24bb0ccf8a4afbc4af4aa3cc3eca0e66282b
Georgiy Filippov <g.filippov777@gmail.com>
2023-06-01 18:38:18 +0500

    feat: add handleChangeInput into form

--- src/components/EmployeesAddForm/EmployeesAddForm.jsx
+++ src/components/EmployeesAddForm/EmployeesAddForm.jsx
@@ -1,32 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styles from './EmployeesAddForm.module.css'
 
-const EmployeesAddForm = () => {
-  return (
-    <section className={styles.wrapper}>
-      <h3 className='mb-3'>Add new Employee</h3>
-      <form className='d-flex gap-4'>
-        <input
-          type='text'
-          className='form-control new-post-label'
-          placeholder="What's his/her Full Name?"
-        />
-        <input
-          type='number'
-          className='form-control new-post-label'
-          placeholder='Salary in $?'
-        />
+class EmployeesAddForm extends Component {
+  state = {
+    fullName: '',
+    salary: '',
+  }
 
-        <button
-          type='submit'
-          style={{ minWidth: '25%' }}
-          className='btn btn-primary'
-        >
-          Add Employee
-        </button>
-      </form>
-    </section>
-  )
+  handleChangeInput = evt => {
+    this.setState({ [evt.target.name]: evt.target.value })
+  }
+
+  render() {
+    const { fullName, salary } = this.state
+
+    return (
+      <section className={styles.wrapper}>
+        <h3 className='mb-3'>Add new Employee</h3>
+        <form className='d-flex gap-4'>
+          <input
+            type='text'
+            name='fullName'
+            className='form-control new-post-label'
+            placeholder="What's his/her Full Name?"
+            value={fullName}
+            onChange={this.handleChangeInput}
+          />
+          <input
+            type='number'
+            name='salary'
+            className='form-control new-post-label'
+            placeholder='Salary in $?'
+            value={salary}
+            onChange={this.handleChangeInput}
+          />
+
+          <button
+            type='submit'
+            style={{ minWidth: '25%' }}
+            className='btn btn-primary'
+          >
+            Add Employee
+          </button>
+        </form>
+      </section>
+    )
+  }
 }
 
 export default EmployeesAddForm
